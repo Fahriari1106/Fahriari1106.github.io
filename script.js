449e7aef317f762d1f7e8d92407bd20ed55b6ed9
Fahriari1106 <57537931+Fahriari1106@users.noreply.github.com>
2020-01-23 09:30:53 +0700

    commit awal

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.carousel').carousel({
+    interval: 3000
+})
+
+$('.slider').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+
+});
